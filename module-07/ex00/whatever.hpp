6716b4b194fa333392c6a7c940b42220d254a474
Zakaria Bengueddime <zbengued@e1r8p10.1337.ma>
2026-03-01 00:06:45 +0000

    add module07 and ex00

---
+++ module-07/ex00/whatever.hpp
@@ -0,0 +1,19 @@
+#pragma  once
+
+template <typename T>
+void	swap(T *x, T *y) {
+	T	tmp;
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+template <typename T>
+T	max(const T &x, const T &y) {
+	return (x < y) ? y : x;
+}
+
+template <typename T>
+T	min(T x, T y) {
+	return (x > y) ? y : x;
+}
