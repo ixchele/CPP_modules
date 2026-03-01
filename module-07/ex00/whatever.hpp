b600b5a461d4efc76c0b054ca89f0699be41e2bb
Zakaria Bengueddime <zbengued@e1r8p10.1337.ma>
2026-03-01 00:23:46 +0000

    fix ex00 mod07

--- module-07/ex00/whatever.hpp
+++ module-07/ex00/whatever.hpp
@@ -1,11 +1,11 @@
 #pragma  once
 
 template <typename T>
-void	swap(T *x, T *y) {
+void	swap(T &x, T &y) {
 	T	tmp;
-	tmp = *x;
-	*x = *y;
-	*y = tmp;
+	tmp = x;
+	x = y;
+	y = tmp;
 }
 
 template <typename T>
